94d98a0a71aec85a095b6215c106e4efbf058717
choitjddn0311 <cswcjw311427@naver.com>
2024-05-30 11:30:15 +0900

    feat : img_modal

--- drag_and_drop/script.js
+++ drag_and_drop/script.js
@@ -28,7 +28,7 @@ const dropbox = document.querySelector("#dropBox")
 						const img = `
 							<div class='photo'>
 								<span onclick='removePhoto(this)'>X</span>
-								<img src='${src}'>
+								<img src='${src}'onclick='scaleImg("${src}")'>
 							</div>
 						`
 						photobox.innerHTML += img
@@ -38,7 +38,19 @@ const dropbox = document.querySelector("#dropBox")
 		}
 
         const removePhoto = (e) => e.parentElement.remove()
+		const imgModal = document.getElementById('imgModal')
+		const modalImg = document.getElementById('modalImage')
         const del = document.getElementById('del')
+		const close = document.getElementById('close')
         del.addEventListener('click' , () => {
             photobox.innerHTML =  '';
         })
+
+		const scaleImg = (src) => {
+			modalImg.src = src
+			imgModal.style.display = "flex"
+		};
+		close.addEventListener('click' , () => {
+			imgModal.style.display = "none"
+		})
+
